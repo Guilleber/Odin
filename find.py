c8021690f277ef2f8c91e4f4550b2fa632efeaa7
Guilleber <Guilleber>
2023-01-31 21:46:49 -0500

    revert to using exiftool

---
+++ find.py
@@ -0,0 +1,15 @@
+import jsondatetime as json
+import argparse
+
+from utils.mongo_tools import MongoInterface
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find files")
+    parser.add_argument('query', metavar='QUERY', type=json.loads,
+                        help="A mongodb query describing what to find")
+
+    args = parser.parse_args()
+
+    mongo = MongoInterface()
+    mongo.find(args.query)
