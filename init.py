8b61d453a16c20563ab3f953d12324a84fba01e9
Guilleber <Guilleber>
2023-02-04 08:41:26 -0500

    restructure

--- init.py
+++
@@ -1,5 +0,0 @@
-from utils.mongo_tools import MongoInterface
-
-
-if __name__ == "__main__":
-    MongoInterface().config()
--- utils/geolocation_tools.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-from typing import Dict, Any
-
-
-geolocator = Nominatim(user_agent="GeoSort")
-
-def locate(lat: float, lng: float) -> Dict[str, str]:
-    location = geolocator.reverse("{:.6f},{:.6f}".format(lat, lng))
-    return location.raw["address"]
